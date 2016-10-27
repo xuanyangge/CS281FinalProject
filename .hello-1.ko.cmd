33237353951222cce669b70dcabc51dba2830b6e
xuanyangge <xuanyangge@gmail.com>
2016-10-27 11:41:32 -0500

    Delete .hello-1.ko.cmd

--- .hello-1.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/vagrant/Desktop/linuxmodule/hello-1.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/vagrant/Desktop/linuxmodule/hello-1.ko /home/vagrant/Desktop/linuxmodule/hello-1.o /home/vagrant/Desktop/linuxmodule/hello-1.mod.o
